9e99e1c7a041898190f2a4b83cac55c1aea59ce0
Ilsur Akhmetshin <niks.art@mail.ru>
2022-04-17 19:28:21 +0300

    fix: calculator connect to js

---
+++ L4/script.js
@@ -0,0 +1,12 @@
+function makeSum()
+{
+        num1= document.getElementById("numberone").value;
+        num2= document.getElementById("numbertwo").value;
+    document.getElementById("result").innerHTML = num1 + num2;
+}
+function makeMult()
+{
+        num1= document.getElementById("numberone").value;
+        num2= document.getElementById("numbertwo").value; 
+    document.getElementById("result").innerHTML = num1 * num2;
+}
